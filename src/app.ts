8951949cdbae6625b81043545fa55761087549ff
Conrad Barthelmes <conrad.barthelmes@pro-volution.de>
2021-12-12 12:36:53 +0100

    added typescript, eslint + prettier and ported everthing to typescript

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import config from '../config.json';
-import MqttClient from "./MqttClient";
+import MqttClient from './MqttClient';
 
 import {Gpio} from 'onoff';
 
 if (!Gpio.accessible) {
-    throw new Error("GPIO is unaccessible - are you running on a Raspberry PI?");
+    throw new Error('GPIO is unaccessible - are you running on a Raspberry PI?');
 }
 
 async function run() {
     const client = new MqttClient(config.MQTT.host, config.MQTT.topic);
     await client.connect();
-    const irDetector = new Gpio(config.GPIO_IR, "in");
 
+    const irDetector = new Gpio(config.GPIO_IR, 'in');
     irDetector.watch((err, value) => {
         if (err) {
             console.error(err);
